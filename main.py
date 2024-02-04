d608453bcbddc9b22a123a2492add44fdd288332
Mercy1ok <155935487+Mercy1ok@users.noreply.github.com>
2024-02-04 15:12:40 +0000

    did everything
    nothing

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
-# input statements
-salary = float(input(1250))
-numDependents = float(input())
-statetax = float(input(0.065 * salary))
-federaltax = float(input(0.28))
-dependentDeduction = float(input(numDependents * .025))
-totalwithholding = float(input(statetax + federaltax + dependentDeduction))
-takehomepay = float(input(salary - totalwithholding))
-# calculate taxes here
 
-# output statements
-print("Salary: $" + str(statetax))
+salary = 1250
+numDependents = float(2)
+statetax = float(0.065 * salary)
+federaltax = float(0.28 * salary)
+dependentDeduction = float(salary * (.025 * numDependents))
+totalwithholding = float(statetax + federaltax + dependentDeduction)
+takehomepay = float(salary - totalwithholding)
+rounded = round(federaltax, 2)
+
+print("State Tax: $" + str(statetax))
+print("Federal Tax: $" + str(rounded))
+print("Dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
-print("Take Home Pay: $" + str(takeHomePay))
+print("Total Withholding: $" + str(takehomepay))
+
+print("-----------------------------------------")
+salary = 60000
+numDependents = float(3)
+statetax = float(0.065 * salary)
+federaltax = float(0.28 * salary)
+dependentDeduction = float(salary * (.025 * numDependents))
+totalwithholding = float(statetax + federaltax + dependentDeduction)
+takehomepay = float(salary - totalwithholding)
+rounded1 = round(dependentDeduction, 2)
+rounded = round(federaltax, 2)
+
+print("State Tax: $" + str(statetax))
+print("Federal Tax: $" + str(rounded1))
+print("Dependents: $" + str(rounded))
+print("Salary: $" + str(salary))
+print("Total Withholding: $" + str(takehomepay))
+print("-----------------------------------------")
+
+salary = 30000
+numDependents = float(6)
+statetax = float(0.065 * salary)
+federaltax = float(0.28 * salary)
+dependentDeduction = float(salary * (.025 * numDependents))
+totalwithholding = float(statetax + federaltax + dependentDeduction)
+takehomepay = float(salary - totalwithholding)
+rounded = round(dependentDeduction,2)
+
+print("State Tax: $" + str(statetax))
+print("Federal Tax: $" + str(federaltax))
+print("Dependents: $" + str(rounded))
+print("Salary: $" + str(salary))
+print("Total Withholding: $" + str(takehomepay))
+
